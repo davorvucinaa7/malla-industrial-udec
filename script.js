1af415ff8d2ac813c75ced820b26504c7e845d77
davorvucinaa7 <davorvucinaa7@gmail.com>
2025-07-14 04:32:40 -0400

    script.js

--- script.js
+++ script.js
@@ -32,9 +32,10 @@ const malla = [
                 courses: [
                     { code: "521227", name: "Cálculo III", credits: 5, prereqs: ["525150", "527150"] },
                     { code: "503203", name: "Programación", credits: 3, prereqs: ["525140"] },
-                    { code: "523219", name: "Estadística", credits: 4, prereqs: ["527150", "525140"] },
+                    { code: "523219", name: "Estadística", credits: 4, prereqs: ["525140", "527150"] },
                     { code: "525223", name: "Ecuaciones Diferenciales", credits: 4, prereqs: ["525150", "527150"] },
-                    { code: "890050", name: "Inglés Comunicativo Nivel Básico I", credits: 5, prereqs: [] }
+                    { code: "890050", name: "Inglés Comunicativo Nivel Básico I", credits: 5, prereqs: [] },
+                    { code: "580201", name: "Liderazgo y Trabajo en Equipo", credits: 2, prereqs: ["580120"] }
                 ]
             },
             {
@@ -44,7 +45,8 @@ const malla = [
                     { code: "580211", name: "Modelación de Sistemas", credits: 2, prereqs: ["510140", "525140", "527140", "531140", "580120"] },
                     { code: "521230", name: "Cálculo Numérico", credits: 4, prereqs: ["521227", "503203"] },
                     { code: "541271", name: "Mecánica", credits: 3, prereqs: ["521227", "510150"] },
-                    { code: "541203", name: "Termodinámica", credits: 4, prereqs: ["510150"] }
+                    { code: "541203", name: "Termodinámica", credits: 4, prereqs: ["510150"] },
+                    { code: "523325", name: "Inferencia Estadística y Muestreo", credits: 4, prereqs: ["523219"] }
                 ]
             }
         ]
@@ -55,21 +57,23 @@ const malla = [
             {
                 semester: "5º Semestre",
                 courses: [
-                    { code: "580315", name: "Optimización I", credits: 3, prereqs: ["521227", "503203"] },
                     { code: "541340", name: "Mecánica de Fluidos", credits: 4, prereqs: ["541271", "541203"] },
-                    { code: "580490", name: "Práctica Laboral", credits: 2, prereqs: [] },
                     { code: "543355", name: "Máquinas Eléctricas", credits: 4, prereqs: ["525223"] },
-                    { code: "580310", name: "Microeconomía", credits: 4, prereqs: ["523219"] }
+                    { code: "580310", name: "Microeconomía", credits: 4, prereqs: ["523219"] },
+                    { code: "580315", name: "Optimización I", credits: 3, prereqs: ["521227", "503203"] },
+                    { code: "580311", name: "Análisis Estadístico Multivariado", credits: 3, prereqs: ["523325"] },
+                    { code: "580490", name: "Práctica Laboral", credits: 2, prereqs: [] }
                 ]
             },
             {
                 semester: "6º Semestre",
                 courses: [
-                    { code: "580311", name: "Análisis Estadístico Multivariado", credits: 3, prereqs: ["523325"] },
-                    { code: "580325", name: "Optimización II", credits: 3, prereqs: ["580315"] },
-                    { code: "580327", name: "Simulación", credits: 3, prereqs: ["580311", "580315"] },
                     { code: "542355", name: "Transferencia de Calor", credits: 4, prereqs: ["541340"] },
-                    { code: "580321", name: "Administración", credits: 3, prereqs: ["580211"] }
+                    { code: "580321", name: "Administración", credits: 3, prereqs: ["580211"] },
+                    { code: "580325", name: "Optimización II", credits: 3, prereqs: ["580315"] },
+                    { code: "580323", name: "Macroeconomía", credits: 3, prereqs: ["580310"] },
+                    { code: "580327", name: "Simulación", credits: 3, prereqs: ["580311", "580315"] },
+                    { code: "580380", name: "Dibujo Industrial", credits: 3, prereqs: [] }
                 ]
             }
         ]
@@ -80,21 +84,24 @@ const malla = [
             {
                 semester: "7º Semestre",
                 courses: [
-                    { code: "580323", name: "Macroeconomía", credits: 3, prereqs: ["580310"] },
+                    { code: "540451", name: "Procesos Industriales", credits: 3, prereqs: ["542355"] },
+                    { code: "580301", name: "Innovación", credits: 3, prereqs: [] },
                     { code: "580413", name: "Marketing", credits: 3, prereqs: ["580321"] },
                     { code: "580414", name: "Contabilidad General y de Costos", credits: 3, prereqs: ["580321"] },
                     { code: "580415", name: "Gestión Estratégica y Control de Gestión", credits: 4, prereqs: ["580321"] },
-                    { code: "540451", name: "Procesos Industriales", credits: 3, prereqs: ["542355"] }
+                    { code: "201", name: "Complementaria I", credits: 2, prereqs: [] },
+                    { code: "554150", name: "Introducción a la Sustentabilidad en Ingeniería", credits: 3, prereqs: [] }
                 ]
             },
             {
                 semester: "8º Semestre",
                 courses: [
-                    { code: "580423", name: "Gestión y Control de Calidad", credits: 3, prereqs: ["580311"] },
+                    { code: "580421", name: "Planificación y Control de Producción", credits: 4, prereqs: ["580327"] },
                     { code: "580425", name: "Ingeniería Económica", credits: 3, prereqs: ["580414"] },
                     { code: "580429", name: "Finanzas", credits: 4, prereqs: ["580414"] },
-                    { code: "580421", name: "Planificación y Control de Producción", credits: 4, prereqs: ["580327"] },
-                    { code: "201", name: "Complementaria I", credits: 2, prereqs: [] }
+                    { code: "580423", name: "Gestión y Control de Calidad", credits: 3, prereqs: ["580311"] },
+                    { code: "503700", name: "Tecnologías de Información", credits: 4, prereqs: [] },
+                    { code: "202", name: "Complementaria II", credits: 2, prereqs: [] }
                 ]
             }
         ]
@@ -108,8 +115,10 @@ const malla = [
                     { code: "580512", name: "Diseño de Sistemas de Producción", credits: 3, prereqs: ["580315"] },
                     { code: "580513", name: "Evaluación de Proyectos", credits: 3, prereqs: ["580425"] },
                     { code: "580514", name: "Gestión de Personas y Comportamiento Organizacional", credits: 3, prereqs: ["580321"] },
-                    { code: "202", name: "Complementaria II", credits: 2, prereqs: [] },
-                    { code: "580590", name: "Práctica Profesional", credits: 4, prereqs: ["580490"] }
+                    { code: "580590", name: "Práctica Profesional", credits: 4, prereqs: ["580490"] },
+                    { code: "101", name: "Electivo I", credits: 3, prereqs: [] },
+                    { code: "102", name: "Electivo II", credits: 3, prereqs: [] },
+                    { code: "103", name: "Electivo III", credits: 3, prereqs: [] }
                 ]
             },
             {
@@ -118,9 +127,6 @@ const malla = [
                     { code: "580525", name: "Dirección y Control de Proyectos", credits: 3, prereqs: ["580513"] },
                     { code: "580523", name: "Taller de Emprendimiento", credits: 3, prereqs: ["580513"] },
                     { code: "580521", name: "Logística", credits: 3, prereqs: ["580327"] },
-                    { code: "101", name: "Electivo I", credits: 3, prereqs: [] },
-                    { code: "102", name: "Electivo II", credits: 3, prereqs: [] },
-                    { code: "103", name: "Electivo III", credits: 3, prereqs: [] },
                     { code: "104", name: "Electivo IV", credits: 3, prereqs: [] },
                     { code: "105", name: "Electivo V", credits: 3, prereqs: [] },
                     { code: "106", name: "Electivo VI", credits: 3, prereqs: [] }
